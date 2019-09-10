37030f99ddc7674ad40ab567cbfe594e482db6e2
marquesr1 <rafael.fmarques@outlook.com>
2019-09-10 16:19:34 -0300

    Login page

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Button, TextInput, View, StyleSheet } from 'react-native';
+
+import FormRow from '../components/FormRow';
 
 export default class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    onChangeHandler(field, value) {
+        this.setState({
+            [field]: value
+        });
+    }
+
+    processLogin() {}
+
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <FormRow>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder='Email: user@provider.com'
+                        value={this.state.email}
+                        onChangeText={(value) =>
+                            this.onChangeHandler('email', value)
+                        }
+                    />
+                </FormRow>
+                <FormRow>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder='Senha'
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={(value) =>
+                            this.onChangeHandler('password', value)
+                        }
+                    />
+                </FormRow>
+                <Button title='Entrar' onPress={() => this.processLogin()} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    TextInput: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        paddingLeft: 10,
+        paddingRight: 10
+    }
+});
