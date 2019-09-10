d9e93899cfa1cfb4d534381f8647928f36d54db5
marquesr1 <rafael.fmarques@outlook.com>
2019-09-09 22:15:51 -0300

    Header

--- App.js
+++ App.js
@@ -2,11 +2,31 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import LoginScreen from './src/pages/LoginScreen';
 
-const AppNavigator = createStackNavigator({
-    'Login': {
-        screen: LoginScreen
+const AppNavigator = createStackNavigator(
+    {
+        'Login': {
+            screen: LoginScreen,
+            navigationOptions: {
+                title: 'Bem vindo'
+            }
+        }
+    },
+    {
+        defaultNavigationOptions: {
+            title: 'Minhas series',
+            headerTintColor: 'white',
+            headerStyle: {
+                backgroundColor: '#003994',
+                borderBottomWidth: 1,
+                borderBottomColor: '#C5C5C5'
+            },
+            headerTitleStyle: {
+                color: 'white',
+                fontSize: 30
+            }
+        }
     }
-});
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
